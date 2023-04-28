f26bae867bc7998723ec768a6fd55d5e33250b66
LegenQS <qw2360@columbia.edu>
2023-04-28 18:09:57 -0400

    init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ public class Main {
         System.out.println("Hello world!");
         int[] arr = new int[10];
 
-        for (int i = 0; i <= 10; i++) {
+        for (int i = 0; i < 10; i++) {
             arr[i] = i;
         }
     }
